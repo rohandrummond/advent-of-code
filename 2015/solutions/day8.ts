7174fd52324d04ec1cc62bcdfe1a8358350759c2
Rohan Drummond <drummond.rohan@gmail.com>
2025-12-23 20:32:29 +1300

    Complete day 8 part 1

--- 2015/solutions/day8.ts
+++ 2015/solutions/day8.ts
@@ -1,9 +1,31 @@
 import fs from 'node:fs'
 
+function countCharacters(input: string[]) {
+  let count = 0
+  for (const word of input) {
+    const codeLength = word.length
+    let memoryLength = 0
+    for (let i = 1; i < word.length - 1; i++) {
+      if (word[i] === '\\') {
+        if (word[i + 1] === 'x') {
+          i += 3
+        } else {
+          i += 1
+        }
+        memoryLength++
+      } else {
+        memoryLength++
+      }
+    }
+    count += codeLength - memoryLength
+  }
+  return count
+}
+
 try {
-  const input = fs.readFileSync('./inputs/day8example.txt', 'utf8')
+  const input = fs.readFileSync('./inputs/day8.txt', 'utf8')
   const instructions = input.split(/\r?\n/).filter(Boolean)
-  console.log(instructions)
+  console.log(`Total is ${countCharacters(instructions)}`)
 } catch (err: unknown) {
   console.error(err)
 }
